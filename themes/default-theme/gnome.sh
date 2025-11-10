691ff8765b6441c1ae1c3c144208473ed10a38ff
Tomy Dng <huyxdong.dev@gmail.com>
2025-11-10 09:52:11 +0700

    update themes

---
+++ themes/default-theme/gnome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+OMADEBIAN_THEME_COLOR="purple"
+OMADEBIAN_THEME_BACKGROUND="default-theme/background.jpg"
+source $OMADEBIAN_PATH/themes/set-gnome-theme.sh
---
+++ themes/default-theme/vscode.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "VSCode theme settings applied. Do nothing!"
