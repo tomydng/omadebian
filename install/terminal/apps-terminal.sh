6eb7a56a4f09e1f8f714da71aac2f5c5135ba424
Tomy Dng <huyxdong.dev@gmail.com>
2025-11-16 09:19:51 +0700

    update apps-terminal

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install -y tree fzf ripgrep bat eza zoxide plocate apache2-utils fd-find zip unzip direnv git-delta
+sudo apt install -y tree fzf ripgrep bat eza zoxide plocate apache2-utils fd-find zip unzip direnv git-delta ffmpeg
